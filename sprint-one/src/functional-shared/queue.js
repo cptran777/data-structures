d829295e6945562a75ce108d042fc5360b5d7875
cptran777 <cptran14@gmail.com>
2016-06-08 15:36:27 -0700

    Passes all relevant tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = {
+    objectSize: 0,
+    storage: {}
+  };
+
+  _.extend(object, queueMethods);
+
+  return object;
+
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  size: function () {
+    return this.objectSize;
+  },
+  enqueue: function (val) {
+    this.storage[this.objectSize] = val;
+    this.objectSize++;
+  },
+  dequeue: function () {
+    if (this.objectSize > 0) {
+      var result = this.storage[0];
+      this.objectSize--;
+      for ( var i = 1; i <= this.objectSize; i++) {
+        this.storage[i-1] = this.storage[i];
+      }
+    }
+    return result;
+  }
+};
