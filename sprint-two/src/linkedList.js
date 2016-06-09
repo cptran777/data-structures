6ecec4abe58db15c5cbe93953a66fc18554c16bb
cptran777 <cptran14@gmail.com>
2016-06-08 20:09:02 -0700

    linkedlist passes all tests.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var idontknow = list.head.value;
+    list.head = list.head.next;
+    return idontknow;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if(currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +44,6 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ list.contains => O(n) time
+ list.removeHead & list.addToTail => O(1) time
  */
