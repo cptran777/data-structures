fb23fa2fdd0168d59f99791d5772d7814c420313
cptran777 <cptran14@gmail.com>
2016-06-09 11:32:04 -0700

    passes all tests. Filled in time complexity prompt.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ for all functions, O(1) time. 
  */
