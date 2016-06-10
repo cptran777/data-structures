c1ac6a9025595abdae6686b10727ced8520f0cd8
cptran777 <cptran14@gmail.com>
2016-06-09 20:30:26 -0700

    passes all tests, with time complexity analysis.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,59 @@
 var BinarySearchTree = function(value) {
+
+  var obj = {};
+
+  obj.right = null;
+  obj.left = null;
+  obj.value = value;
+
+  obj.contains = function (val) {
+    if (obj.value === val) {
+      return true;
+    } else if (val < obj.value && obj.left !== null) {
+      return obj.left.contains(val);
+    } else if (obj.right !== null) {
+      return obj.right.contains(val);
+    }
+    return false;
+  };
+
+  obj.insert = function (val) {
+    //var newTree = BinarySearchTree(val);
+    if (val < obj.value) {
+      if (obj.left === null) {
+        var newTree = BinarySearchTree(val);
+        obj.left = newTree;
+      } else {
+        obj.left.insert(val);
+      }
+    } else if (val > obj.value) {
+      if (obj.right === null) {
+        var newTree = BinarySearchTree(val);
+        obj.right = newTree;
+      } else {
+        obj.right.insert(val);
+      }
+    }
+  };
+
+  obj.depthFirstLog = function (callback) {
+    callback(obj.value);
+    if (obj.left !== null) {
+      obj.left.depthFirstLog(callback);
+    }
+    if (obj.right !== null) {
+      obj.right.depthFirstLog(callback);
+    }
+  };
+
+  return obj;
 };
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * contains => O(log n)
+ * insert => O(log n)
+ * depthFirstLog => O(n)
  */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,66 @@
 
 
+
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, {});
+  }
+  this._storage.get(index)[k] = v;
+  this._count++;
+
+  if (this._count >= .75 * this._limit) {
+    this.resize(2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var testVar = this._storage.get(index);
+  delete this._storage.get(index)[k];
+  this._count--;
+  if (this._count <= .25 * this._limit && (this._limit > 8)) {
+    this.resize(.5);
+  }
+};
+
+HashTable.prototype.resize = function(scalar) {
+  this._limit = this._limit * scalar;
+  var newTable = LimitedArray(this._limit);
+  var self = this;
+  this._storage.each(function(item, index) {
+    if (item !== undefined) {
+      for (var key in item) {
+        var newIndex = getIndexBelowMaxForKey(key, self._limit);
+        if (newTable.get(newIndex) === undefined) {
+          newTable.set(newIndex, {});
+        }
+        newTable.get(newIndex)[key] = item[key];
+      }
+    }
+  });
+  this._storage = newTable;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert => O(1)
+ * retrieve => O(1)
+ * remove => O(1)
+ * resize => O(n)
  */
 
 
