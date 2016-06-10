24dfa66c4823d7000a47f06ba9c253223ed343e7
cptran777 <cptran14@gmail.com>
2016-06-10 16:16:06 -0700

    contains functionality to reference parent, remove children from parent, and traverse each value of the tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
   // your code here
   newTree.children = [];  // fix me
@@ -12,7 +13,9 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push(Tree(value));
+  var babyTree = Tree(value);
+  babyTree.parent = this;
+  this.children.push(babyTree);
 };
 
 treeMethods.contains = function(target) {
@@ -27,10 +30,30 @@ treeMethods.contains = function(target) {
   return false;
 };
 
+treeMethods.removeFromParent = function () {
+  var parent = this.parent;
+  this.parent = null;
+  for (var x = 0; x < parent.children.length; x++) {
+    if (parent.children[x] === this) {
+      return parent.children.splice(x, 1)[0]; 
+    }
+  }
+};
+
+treeMethods.traverse = function (callback) {
+  var val = callback(this.value);
+  this.value = val === undefined ? val : this.value;
+  this.children.forEach(function traverseChildren (child) {
+    child.traverse(callback);
+  });
+};
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  * addChild => O(1)
  * contains => O(n)
+ * removeFromParent => O(n)
+ * traverse => O(n);
  */
